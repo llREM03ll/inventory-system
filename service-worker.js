1979c47bcfbe8b5763c2c6fac4702f5316f4872e
llREM03ll <rembenedito@gmail.com>
2025-08-19 01:58:53 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "brews-inventory-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./style.css",
+  "./script.js"
+];
+
+// Install & cache files
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Serve from cache when offline
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
